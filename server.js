40175b2656ff4ef169a51a6576545915ad205836
Amoxicillin <1922351498@qq.com>
2022-04-08 23:39:28 +0800

    MySQL增删改查完毕

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 let KoaRouter = require('koa-router')
+const mysql = require("mysql")
 const fs = require('fs')
 const path = require('path')
 
@@ -8,142 +9,171 @@ const app = new Koa()
 const router = new KoaRouter()
 app.use(bodyParser())
 
-// // 增删改查接口
+//连接数据库
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'tlt361989',
+	port: '3306',
+	database: 'miniprogram'
+})
 
-//新增和修改
-router.post('/add-shoppingCart', async (ctx) => {
-	// console.log(ctx.request.body);
-	if (ctx.request.body.id) {
-		let id = ctx.request.body.id
-		let params = ctx.request.body
-		let writeJson = () => {
-			return new Promise((resolve,reject)=>{
-					fs.readFile(path.join(__dirname, '/datas/shoppingCart.json'),function(err,data){
-							if(err){
-							// 报错返回
-									resolve({code: -1, msg: '新增失败' + err})
-									return console.error(err);
-							}
-							let jsonData = data.toString()
-							jsonData = JSON.parse(jsonData)
-
-							let itemIndex = jsonData.findIndex(item=>item.id === id)
-							if (itemIndex>=0) {
-								// jsonData.splice(jsonData.findIndex(item => item.id === id), 1, params)
-								//已经有的数量+1
-								jsonData[itemIndex].count+=1
-							} else {
-								//没有就新增
-								params.count = 1
-								params.selected = true
-								jsonData.push(params)
-							}
-							let str = JSON.stringify(jsonData)
-							fs.writeFile(path.join(__dirname, '/datas/shoppingCart.json'),str,function(err){
-									if(err){
-											resolve({code: -1, msg: '新增失败！' + err})
-									}
-									resolve({code: 0, msg: '新增成功！'})
-							})
-					})
+//新增数据
+router.post('/add', async (ctx, next) => {
+		console.log(JSON.stringify(ctx.request))
+		var data = {}
+		var querySql="INSERT INTO shoppingcart VALUES(?,?,?,?,?,?,?,?,?,?,?)"
+		var queryParams=[
+			ctx.request.body.count,
+			ctx.request.body.showIndex,
+			ctx.request.body.superCategoryId,
+			ctx.request.body.selected,
+			ctx.request.body.promTag,
+			ctx.request.body.wapBannerUrl,
+			ctx.request.body.name,
+			ctx.request.body.retailPrice,
+			ctx.request.body.weight,
+			ctx.request.body.id,
+			ctx.request.body.describe
+		]
+		var searchSql = (querySql,queryParams) => {
+			return new Promise((resolve, reject) => {
+				console.log('准备新增')
+				connection.query(querySql,queryParams, function(err, result) {
+					if(err){
+						reject(0)
+						console.log(err+'err')
+					}
+					resolve(result)
+				})
 			})
 		}
-		// 返回给前端
-		ctx.body = await writeJson()
+		var res= await searchSql(querySql,queryParams)
+		if(res){
+			data.code='0'
+			data.msg="添加成功"
+			ctx.body=data
+		}else{
+			data.code='1'
+			data.msg="添加失败"
+			ctx.body=data
+		}
+});
+//查询数据
+router.get('/query', async (ctx, next) => {
+	var data = {}
+	var querySql = "select * from buysdata"
+	var querySql2 = "select * from subcatelist"
+	var querySql3 = "select * from shoppingcart"
+	var searchSql = (thesql) => {
+		return new Promise((resolve, reject) => {
+			console.log('准备查询')
+			connection.query(thesql, function(err, result) {
+				if(err){
+					reject(0)
+					console.log(err+'err')
+				}
+				resolve(result)
+			})
+		})
+	}
+	var res= await searchSql(querySql)
+	var res2= await searchSql(querySql2)
+	var res3= await searchSql(querySql3)
+	if(res&&res2){
+		var param={buysdata:res,subcatelist:res2,shoppingcart:res3}
+		data.code='0'
+		data.msg="成功获取"
+		data.data=param
+		ctx.body=data
+	}
+})
+//更新数据
+router.post('/update', async (ctx, next) => {
+	if (ctx.request.body.isAll) {
+		var updateSql="UPDATE shoppingcart SET selected = '"+ctx.request.body.selected+"'"
+		var searchSql = (thesql) => {
+			return new Promise((resolve, reject) => {
+				connection.query(thesql, function(err, result) {
+					if(err){
+						reject(1)
+						console.log(err+'err')
+					}
+					resolve(0)
+				})
+			})
+		}
+		var data={}
+		var res= await searchSql(updateSql)
+		if(res==0){
+			data.msg="更新成功"
+			data.code=0
+			ctx.body=data
+		}else{
+			data.code=1
+			data.msg="更新失败,请稍后再试！"
+			ctx.body=data
+		}
 	}else{
-		console.log(ctx.request.body.type)
-		let params = ctx.request.body
-		let writeJson = () => {
-			return new Promise((resolve,reject)=>{
-					fs.readFile(path.join(__dirname, '/datas/shoppingCart.json'),function(err,data){
-							if(err){
-							// 报错返回
-									resolve({code: -1, msg: '新增失败' + err})
-									return console.error(err);
-							}
-							let jsonData = data.toString()
-							jsonData = JSON.parse(jsonData)
-
-							if (params.type) {
-								jsonData[params.index].count+=1
-							}else {
-								jsonData[params.index].count>1 ? jsonData[params.index].count-=1 : jsonData.splice(params.index,1)
-							}
-							let str = JSON.stringify(jsonData)
-							fs.writeFile(path.join(__dirname, '/datas/shoppingCart.json'),str,function(err){
-									if(err){
-											resolve({code: -1, msg: '新增失败！' + err})
-									}
-									resolve({code: 0, msg: '新增成功！'})
-							})
-					})
+		var updateSql="UPDATE shoppingcart SET count = '"+ctx.request.body.count+ "' where id = '" +ctx.request.body.id + "'"
+		var updateSql2="UPDATE shoppingcart SET selected = '"+ctx.request.body.selected+ "' where id = '" +ctx.request.body.id + "'"
+		var searchSql = (thesql) => {
+			return new Promise((resolve, reject) => {
+				connection.query(thesql, function(err, result) {
+					if(err){
+						reject(1)
+						console.log(err+'err')
+					}
+					resolve(0)
+				})
 			})
 		}
-		// 返回给前端
-		ctx.body = await writeJson()
+		var data={}
+		var res= await searchSql(updateSql)
+		var res2= await searchSql(updateSql2)
+
+		if(res==0||res2==0){
+			data.msg="更新成功"
+			data.code=0
+			ctx.body=data
+		}else{
+			data.code=1
+			data.msg="更新失败,请稍后再试！"
+			ctx.body=data
+		}
 	}
 	
 	
-})
-//选择
-router.post('/select-shoppingCart', async (ctx) => {
-		let params = ctx.request.body
-		let selectJson = () => {
-			return new Promise((resolve,reject)=>{
-					fs.readFile(path.join(__dirname, '/datas/shoppingCart.json'),function(err,data){
-							if(err){
-							// 报错返回
-									resolve({code: -1, msg: '新增失败' + err})
-									return console.error(err);
-							}
-							let jsonData = data.toString()
-							jsonData = JSON.parse(jsonData)
-
-							jsonData[params.index].selected = params.selected
-
-							let str = JSON.stringify(jsonData)
-							fs.writeFile(path.join(__dirname, '/datas/shoppingCart.json'),str,function(err){
-									if(err){
-											resolve({code: -1, msg: '失败！' + err})
-									}
-									resolve({code: 0, msg: '成功！'})
-							})
-					})
+});
+//删除数据
+router.post('/remove', async (ctx, next) => {
+	var deleteSql="DELETE FROM shoppingcart WHERE "+"id='"+ctx.request.body.id + "'"
+	var searchSql = (thesql) => {
+		return new Promise((resolve, reject) => {
+			connection.query(thesql, function(err, result) {
+				if(err){
+					reject(1)
+					console.log(err+'err')
+				}
+				resolve(0)
 			})
-		}
-		// 返回给前端
-		ctx.body = await selectJson()
+		})
+	}
+	var data={}
+	var res= await searchSql(deleteSql)
 	
-})
-//全选
-router.post('/selectAll-shoppingCart', async (ctx) => {
-			let allSelected = ctx.request.body.allSelected
-			let selectAllJson = () => {
-				return new Promise((resolve,reject)=>{
-						fs.readFile(path.join(__dirname, '/datas/shoppingCart.json'),function(err,data){
-								if(err){
-								// 报错返回
-										resolve({code: -1, msg: '新增失败' + err})
-										return console.error(err);
-								}
-								let jsonData = data.toString()
-								jsonData = JSON.parse(jsonData)
-	
-								jsonData.forEach(element => element.selected = allSelected)
-	
-								let str = JSON.stringify(jsonData)
-								fs.writeFile(path.join(__dirname, '/datas/shoppingCart.json'),str,function(err){
-										if(err){
-												resolve({code: -1, msg: '失败！' + err})
-										}
-										resolve({code: 0, msg: '成功！'})
-								})
-						})
-				})
-			}
-			// 返回给前端
-			ctx.body = await selectAllJson()
-	})
+	if(res==0){
+		data.msg="删除成功"
+		data.code=0
+		ctx.body=data
+	}else{
+		data.code=1
+		data.msg="删除失败,请稍后再试！"
+		ctx.body=data
+	}
+});
+
+
 
 
 router.get('/', (ctx, next) => {
@@ -219,14 +249,13 @@ router.get('/getBuysData', (ctx, next) => {
 
 //获取购物车数据
 let getShoppingData =  require('./datas/shoppingCart.json');
+const { log } = require('console')
 router.get('/getShoppingData', (ctx, next) => {
 	ctx.body = {
 		code: 0,
 		data: getShoppingData
 	};
 });
-
-
 
 
 // 装载所有子路由
